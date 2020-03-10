060936e3a95a7b1630a58dfd1c9cc93c76adc1fd
XieWei <wuxiaoshen@shu.edu.cn>
2020-03-10 13:32:48 +0800

    :rocket: v0.1

--- unsafe_test.go
+++ unsafe_test.go
@@ -2,6 +2,7 @@ package gohelper
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"unsafe"
 )
@@ -20,11 +21,17 @@ func TestNewUnsafeAction(t *testing.T) {
 	}
 	un := NewUnsafeAction(model)
 	mm := un.model.(Model)
+	typ := reflect.TypeOf(mm)
+	for i := 0; i < typ.NumField(); i++ {
+		fmt.Println(typ.Field(i).Name, typ.Field(i).Offset)
+	}
 	unPointer := unsafe.Pointer(&mm)
 	m := uintptr(unPointer) + unsafe.Offsetof(mm.Name)
 	fmt.Println(*(*string)(unsafe.Pointer(m)))
 	age := uintptr(unPointer) + unsafe.Offsetof(mm.Age)
-	fmt.Println(*(*int)(unsafe.Pointer(age)))
+	//age := uintptr(unPointer) + unsafe.Sizeof(8)*2
+	fmt.Println(unsafe.Sizeof(8)*2, unsafe.Offsetof(mm.Age), *(*int)(unsafe.Pointer(age)))
+
 	slice := uintptr(unPointer) + unsafe.Offsetof(mm.Slice)
 	fmt.Println(*(*[]string)(unsafe.Pointer(slice)))
 	length := slice + uintptr(8)
