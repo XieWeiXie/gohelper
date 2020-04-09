23425dea35e926f0d8b34357b1acfcf2ccfbea8d
XieWei <xiewei@XieWeideMacBook-Air.local>
2020-04-09 14:32:25 +0800

    :rocket: v0.1

--- hash_test.go
+++ hash_test.go
@@ -1,6 +1,8 @@
 package gohelper
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
 	"fmt"
 	"strings"
 	"testing"
@@ -11,4 +13,17 @@ func TestNewHash(t *testing.T) {
 	fmt.Println(h.Equal("2472dcd681b761501fadb35ec361503efd27de2ba2270b2fe35cb6ece7362243"))
 	fmt.Println(strings.EqualFold("SSHA256", SHA256))
 
+}
+
+func TestHash(t *testing.T) {
+	fmt.Println(computeAcceptKey("w4v7O6xFTi36lq3RNcgctw=="))
+}
+
+var keyGUID = []byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+
+func computeAcceptKey(challengeKey string) string {
+	h := sha1.New()
+	h.Write([]byte(challengeKey))
+	h.Write(keyGUID)
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
--- ip_test.go
+++ ip_test.go
@@ -6,14 +6,14 @@ import (
 	"testing"
 )
 
-func TestIP(t *testing.T){
-	ip := NewIP(byte(10),byte(100),byte(122),byte(2))
+func TestIP(t *testing.T) {
+	ip := NewIP(byte(10), byte(109), byte(48), byte(95))
 	fmt.Println(ip.Mask().String())
 	IpNet := net.IPNet{
 		IP:   ip.ip,
 		Mask: ip.Mask(),
 	}
 	fmt.Println(IpNet.String())
-	i, n, _ :=net.ParseCIDR("16.158.165.91/22")
-	fmt.Println(i.String(),i.DefaultMask().String(), n.String())
+	i, n, _ := net.ParseCIDR("16.158.165.91/22")
+	fmt.Println(i.String(), i.DefaultMask().String(), n.String())
 }
